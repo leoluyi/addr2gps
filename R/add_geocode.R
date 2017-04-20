9b34188ea70e1bfe1d7ca92e539f35f653a5aef6
LU YI <tsb1050391@gmail.com>
2017-04-20 18:48:48 +0800

    Add add_geocode function

---
+++ R/add_geocode.R
@@ -0,0 +1,29 @@
+#' Add GIS columns to data.frame
+#' 
+#' @param data data.frame contains address variable.
+#' @param addr_var Character. Name of address variable.
+#'
+#' @describeIn geocode Add GIS columns to data.frame.
+#' 
+#' @examples 
+#' dt <- data.table(addrs = c("台北市中正區羅斯福路一段２號",
+#'                            "台北市中正區貴陽街一段１２０號"),
+#'                  X = c(3, 2))
+#' out <- dt %>% add_geocode(addr_var = "addrs", use_tor = FALSE)
+#' out
+#' 
+#' @export
+add_geocode <- function(data, addr_var, precise = FALSE, source = "google", parallel = TRUE, n_cpu = -1L, 
+                        rate = 200, use_tor = TRUE) {
+  
+  setDT(data)
+  
+  addrs <- data[[addr_var]] %>%
+    geocode(precise = precise, source = source, n_cpu = n_cpu, 
+            rate = rate, use_tor = use_tor) %>% 
+    .[, .(addr, lat_y = lat, lon_x = lng)]
+  out <- addrs[data, on = c(addr = addr_var)]
+  out[]
+}
+
+
