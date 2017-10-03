afbcf773b0064a271c6fe25f823737ac6a77c7f6
LU YI <tsb1050391@gmail.com>
2017-10-03 15:15:46 +0800

    Add package documentation

---
+++ R/geocode-package.R
@@ -0,0 +1,8 @@
+#' geocode: A package for computating the notorious bar statistic.
+#' 
+#' The geocode package provides two categories of important functions:
+#' \code{\link{geocode}}, and \code{\link{clean_addr}}.
+#' 
+#' @docType package
+#' @name geocode-package
+NULL
